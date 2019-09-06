8ff7c0046112e9394806f7961f793109ca3aa4cf
BoNo <borcsok.nora@gmail.com>
2019-09-06 16:22:18 +0200

    - undo changes in EditMoodViewModelFactory as it produced a crash.

--- src/main/java/com/example/moodtracker/EditMoodViewModelFactory.java
+++ src/main/java/com/example/moodtracker/EditMoodViewModelFactory.java
@@ -3,6 +3,7 @@ package com.example.moodtracker;
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.example.moodtracker.MoodDatabase.MoodDatabase;
 
@@ -20,15 +21,9 @@ public class EditMoodViewModelFactory extends ViewModelProvider.NewInstanceFacto
         mEntryId = entryId;
     }
 
-    @SuppressWarnings("unchecked")  // To suppress unchecked cast warning (as we catch exception if unknown ViewModel is passed as an argument)
     @NonNull
     @Override
     public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
-        if (modelClass.isAssignableFrom(EditMoodViewModelFactory.class)) {
             return (T) new EditMoodViewModel(mDb, mEntryId);
-        }
-        throw new IllegalArgumentException("Unknown ViewModel class");
-
-
     }
 }
